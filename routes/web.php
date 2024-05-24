29313bf7d80ad8217fad31d7447716887d5a71bf
MitsruYakushinji <156178833+MitsruYakushinji@users.noreply.github.com>
2024-05-24 20:10:36 +0900

    parameter

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,39 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// サンプルページを追加
+// Route::get('/hello', function() {
+//     return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
+// });
+
+// ヒアドキュメント
+Route::get('/hello/{msg?}', function($msg='no message, sorry'){
+
+
+$html = <<<EOF
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello</title>
+    <style>
+        body { font-size: 16pt; color: #999; }
+        h1 { font-size: 100pt; text-align: right; color: #eee; margin: -40px 0px -50px 0px; }
+    </style>
+</head>
+<body>
+    <h1>Hello</h1>
+    <p>{$msg}</p>
+    <p>これはサンプルページです。</p>
+</body>
+</html>
+EOF;
+
+return $html;
+
+});
+// Route::get('/hello', function() use ($html) {
+//     return $html;
+// });
